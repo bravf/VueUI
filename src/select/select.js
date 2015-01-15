42c1d769aaf479a7aaf1f321acfc5cedc50255ba
bravf <bravfing@126.com>
2015-01-15 14:03:15 +0800

    当组件距离页面底部的距离不够展示options时候向上展开

--- src/select/select.js
+++ src/select/select.js
@@ -29,6 +29,46 @@ VueUI.component('vue-select', {
     methods : {
         toggleOptions : function (){
             this.display = this.display == 'none' ? 'block' : 'none'
+
+            if (this.display == 'none'){
+                return false
+            }
+
+            var $select = $(this.$el)
+            var $div = $select.find('.vue-select-options-div')
+            var $btn = $select.find('.vue-select-btn')
+            var $ul = $select.find('ul')
+
+            //得到页面总高度
+            var pageH = document.documentElement.scrollHeight
+            //得到组件y轴位置
+            var selectY = $select.position().top
+            //得到容器高度
+            var divH = $div.outerHeight()
+            //得到btn的高度
+            var btnH = $btn.outerHeight()
+
+            var scrollTop = 0
+            var marginTop = 0
+            var fn = ''
+
+            if (divH >= (pageH-selectY)){
+                marginTop = -(btnH + divH + 2)
+                scrollTop = 1e4
+                fn = 'addClass'
+            }
+            else {
+                marginTop = 0
+                scrollTop = 0
+                fn = 'removeClass'
+            }
+
+            $div.css('margin-top', marginTop)
+            $ul[fn]('vue-select-options-ul-scaleY')
+
+            Vue.nextTick(function (){
+                $div.scrollTop(scrollTop)
+            })
         },
         buttonClick : function (){
             this.toggleOptions()
