ddf16c579e80b2c50ac97ef957dc3a75ca5a99fc
bravf <bravfing@126.com>
2015-01-18 18:59:47 +0800

    制作中...

--- src/table/table.js
+++ src/table/table.js
@@ -3,12 +3,28 @@
 */
 
 VueUI.component('vue-table', {
-    template : '',
+    template :
+        '<table class="table table-condensed table-bordered table-hover">' +
+            '<thead><tr>' +
+                '<th v-repeat="c:columns">{{c["text"]}}</th>' +
+            '</tr></thead>' +
+            '<tbody>' +
+                '<tr v-repeat="d:data">' +
+                    '<td v-repeat="c:columns">{{d[c["field"]]}}</td>' +
+                '</tr>' +
+            '</tbody>' +
+            '<tfoot><tr><td colspan="{{columns.length}}" class="vue-table-pager-td">' +
+                '<vue-pager v-ref="pager"></vue-pager>' +
+            '</td></tr></tfoot>' +
+        '</table>'
+    ,
     data : function (){
         return {
             config : {},
             //数据
-            data : []
+            data :[],
+            //列
+            columns : []
         }
     },
     watch : {
@@ -16,5 +32,18 @@ VueUI.component('vue-table', {
     methods : {
     },
     compiled : function (){
+        //处理columns
+        var columns = this.columns
+        if (columns.length){
+            for (var i=0,c; i<columns.length; i++){
+                c = columns[i]
+                if ($.type(c) == 'string'){
+                    columns[i] = {'field':c, 'text':c}
+                }
+            }
+        }
+        //处理pager
+        console.log(this.$.pager)
+        this.$.pager.totalPage = 100
     }
 })
