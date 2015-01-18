7a64d63dbb59bad0484c9a3ea902975b7f2e1950
bravf <bravfing@126.com>
2015-01-18 15:54:11 +0800

    增加table组件

---
+++ src/table/table.js
@@ -0,0 +1,20 @@
+/*
+    Copyright (c) 2015 bravf(bravfing@126.com)
+*/
+
+VueUI.component('vue-table', {
+    template : '',
+    data : function (){
+        return {
+            config : {},
+            //数据
+            data : []
+        }
+    },
+    watch : {
+    },
+    methods : {
+    },
+    compiled : function (){
+    }
+})
