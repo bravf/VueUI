d605bd2e445451ff5a0915397efea7e0b0bda63b
bravf <bravfing@126.com>
2015-01-19 12:21:14 +0800

    完成table初步功能

--- src/table/table.js
+++ src/table/table.js
@@ -5,15 +5,15 @@
 VueUI.component('vue-table', {
     template :
         '<table class="table table-condensed table-bordered table-hover">' +
-            '<thead><tr>' +
-                '<th v-repeat="c:columns">{{c["text"]}}</th>' +
+            '<thead v-if="isShowHead"><tr>' +
+                '<th v-repeat="c:columns" v-style="width:c.width, text-align:c.textAlign">{{c["text"]}}</th>' +
             '</tr></thead>' +
             '<tbody>' +
                 '<tr v-repeat="d:data">' +
-                    '<td v-repeat="c:columns">{{d[c["field"]]}}</td>' +
+                    '<td v-repeat="c:columns" v-style="text-align:c.textAlign">{{d[c["field"]]}}</td>' +
                 '</tr>' +
             '</tbody>' +
-            '<tfoot><tr><td colspan="{{columns.length}}" class="vue-table-pager-td">' +
+            '<tfoot v-if="isShowFoot"><tr><td colspan="{{columns.length}}" class="vue-table-pager-td">' +
                 '<vue-pager v-ref="pager" v-with="config:pagerConfig"></vue-pager>' +
             '</td></tr></tfoot>' +
         '</table>'
@@ -23,6 +23,8 @@ VueUI.component('vue-table', {
             config : {},
             data : [], //数据
             columns : [], //列
+            isShowHead : true, //是否显示表格头
+            isShowFoot : true, //是否显示表格尾
 
             //分页相关参数
             pagerConfig : {
@@ -42,17 +44,49 @@ VueUI.component('vue-table', {
         }
     },
     methods : {
-    },
-    compiled : function (){
-        //处理columns
-        var columns = this.columns
-        if (columns.length){
-            for (var i=0,c; i<columns.length; i++){
-                c = columns[i]
-                if ($.type(c) == 'string'){
-                    columns[i] = {'field':c, 'text':c}
+        _handleColumns : function (){
+            function getDefaultConf(){
+                return {
+                    width : 150,
+                    textAlign : 'center'
                 }
             }
+
+            var columns = this.columns
+            if (!columns.length){
+                return
+            }
+
+
+            var me = this
+            var hasAuto = false //检查是否有width=auto的列，不然就设置最后一项width=auto
+
+            for (var i=0,c,len=columns.length; i<len; i++){
+                c = columns[i]
+
+                if ($.type(c) == 'string'){
+                    c = columns[i] = {'field':c, 'text':c}
+                }
+                c = columns[i] = $.extend(true, getDefaultConf(), columns[i])
+
+                if (!c.text){
+                    c.text = c.field
+                }
+
+                if (c.width == 'auto'){
+                    hasAuto = true
+                }
+                else {
+                    c.width = c.width + 'px'
+                }
+            }
+
+            if (!hasAuto){
+                columns[len-1].width = 'auto'
+            }
         }
+    },
+    compiled : function (){
+        this._handleColumns()
     }
 })
