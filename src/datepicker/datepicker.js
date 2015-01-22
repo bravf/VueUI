ee7cfbdc0988e6a6b63b1ee9e4d259dcf25997e4
bravf <bravfing@126.com>
2015-01-22 12:27:56 +0800

    最近比较忙，写的断断续续...

--- src/datepicker/datepicker.js
+++ src/datepicker/datepicker.js
@@ -4,8 +4,25 @@
 
 VueUI.component('vue-datepicker', {
     template :
-        '<div>' +
-            '<span v-repeat="d:dateRange">{{d.text}}</span>' +
+        '<div class="vue-datepicker">' +
+            '<div class="vue-datepicker-inner">' +
+                '<div class="vue-datepicker-head">' +
+                    '<div class="vue-datepicker-label">选择日期</div>' +
+                '</div>' +
+                '<div class="vue-datepicker-body">' +
+                    '<div class="vue-datepicker-ctrl">' +
+                        '<i class="vue-datepicker-preMonthBtn"><</i>' +
+                        '<i class="vue-datepicker-nextMonthBtn">></i>' +
+                        '<p>2014-12-12</p>' +
+                    '</div>' +
+                    '<div class="vue-datepicker-weekRange">' +
+                        '<span v-repeat="w:weekRange">{{w}}</span>' +
+                    '</div>' +
+                    '<div class="vue-datepicker-dateRange">' +
+                        '<span v-repeat="d:dateRange" v-class="d.sclass" v-on="click:itemClick(d.date)">{{d.text}}</span>' +
+                    '</div>' +
+                '</div>' +
+            '</div>' +
         '</div>'
     ,
     data : function (){
@@ -13,6 +30,7 @@ VueUI.component('vue-datepicker', {
         return {
             config : {},
             value : '',
+            weekRange : ['一', '二', '三', '四', '五', '六', '日'],
             dateRange : [], //需要绘制的日期区间
             currYear : today.getFullYear(), //当前年
             currMonth : today.getMonth(), //当前月
@@ -21,6 +39,9 @@ VueUI.component('vue-datepicker', {
     },
     watch : {},
     methods : {
+        itemClick : function (date){
+            console.log(this.stringify(date))
+        },
         stringify : function (date, format){
             format = format || 'yyyy-MM-dd'
 
@@ -41,15 +62,6 @@ VueUI.component('vue-datepicker', {
             return isNaN(date.getFullYear()) ? new Date : date
         },
         getDayCount : function (year, month){ //得到每月总天数
-            if (month < 0){
-                year--
-                month = 0
-            }
-            else if (month > 11){
-                year++
-                month = 0
-            }
-
             var dict = [31,28,31,30,31,30,31,31,30,31,30,31]
 
             //如果2月
@@ -72,21 +84,55 @@ VueUI.component('vue-datepicker', {
             var dayCount = this.getDayCount(this.currYear, this.currMonth)
 
             //如果需要补足上月
-            if (firstDayWeek < 7){
+            if (firstDayWeek > 1){
+                var preMonthYear = this.currYear
+                var preMonth = this.currMonth - 1
+
+                if (preMonth < 0){
+                    preMonth = 11
+                    preMonthYear--
+                }
+
                 //上月总天数
-                var prevMonthDayCount = this.getDayCount(this.currYear, this.currMonth-1)
-                for (var i=0; i<firstDayWeek; i++){
+                var prevMonthDayCount = this.getDayCount(preMonthYear, preMonth)
+                for (var i=2; i<=firstDayWeek; i++){
+                    var dayText = prevMonthDayCount - firstDayWeek + i
                     this.dateRange.push({
-                        text : prevMonthDayCount-firstDayWeek + i + 1
+                        text : dayText,
+                        date : new Date(preMonthYear, preMonth, dayText),
+                        sclass : 'vue-datepicker-item-gray'
                     })
                 }
             }
 
             //本月
             for (var i=1; i<=dayCount; i++){
+                var date = new Date(this.currYear, this.currMonth, i)
+                var week = date.getDay()
                 this.dateRange.push({
-                    text : i
+                    text : i,
+                    date : date,
+                    sclass : ( week==6 || week==0 ) ? 'vue-datepicker-item-red' : ''
                 })
+            }
+
+            //如果需要补足下个月
+            if (this.dateRange.length < 42){
+                //需要补足的天数
+                var nextMonthNeed = 42 - this.dateRange.length
+                var nextMonthYear = this.currYear
+                var nextMonth = this.currMonth + 1
+                if (nextMonth > 11){
+                    nextMonth = 0
+                    nextMonthYear++
+                }
+                for (var i=1; i<=nextMonthNeed; i++){
+                    this.dateRange.push({
+                        text : i,
+                        date : new Date(nextMonthYear, nextMonth, i),
+                        sclass : 'vue-datepicker-item-gray'
+                    })
+                }
             }
         }
     },
