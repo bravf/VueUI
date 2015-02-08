121c2c1a0ab53109b0d246b58f8ecc4e863a04b3
bravf <bravfing@126.com>
2015-02-08 15:25:31 +0800

    update

--- dist/vueUI.js
+++ dist/vueUI.js
@@ -9,7 +9,7 @@ window.VueUI = function (){
     var comCounter = 1
 
     function getComId(){
-        return (new Date).getTime()/1000 + '_' + comCounter
+        return (new Date).getTime() + '_' + comCounter
     }
 
     //将外部传进来的参数mix到data
@@ -84,140 +84,12 @@ window.VueUI = function (){
         Vue.component(key, options)
     }
 
-    VueUI.getComponent = function (id){
+    VueUI.getComponent = VueUI.$ = function (id){
         return componentPool[id]
     }
 
     return VueUI
 
-}()
-/*
-    Copyright (c) 2015 bravf(bravfing@126.com)
-*/
-
-VueUI.component('vue-modal', {
-    template :
-        '<div class="modal vue-modal" v-show="toggle">' +
-            '<div class="modal-backdrop fade in vue-modal-backdrop"></div>' +
-            '<div class="modal-dialog" v-style="width:width+\'px\'">' +
-                '<div class="modal-content">' +
-                    '<div class="modal-header">' +
-                        '<button type="button" class="close" v-on="click:toggle=false"><span aria-hidden="true">×</span></button>' +
-                        '<h4 class="modal-title">{{title}}</h4>' +
-                    '</div>' +
-                    '<div class="modal-body">' +
-                        '{{{content}}}' +
-                        '<content></content>' +
-                    '</div>' +
-                    '<div class="modal-footer" v-show="isShowCancelBtn || isShowOkBtn">' +
-                        '<button type="button" class="btn btn-default" v-on="click:cancelBtnClick" v-show="isShowCancelBtn">{{cancelBtnText}}</button>' +
-                        '<button type="button" class="btn btn-primary" v-on="click:okBtnClick" v-show="isShowOkBtn">{{okBtnText}}</button>' +
-                    '</div>' +
-                '</div>' +
-            '</div>' +
-        '</div>'
-    ,
-    data : function (){
-        return {
-            config : {},
-
-            title : '', //标题
-            content : '', //内容
-            toggle : false, //是否显示
-            width : 500, //宽度
-
-            // 按钮相关
-            isShowCancelBtn : false,
-            cancelBtnText : '取消',
-            cancelBtnCallback : VueUI.emptyFunc,
-
-            isShowOkBtn : false,
-            okBtnText : '确认',
-            okBtnCallback : VueUI.emptyFunc,
-        }
-    },
-    watch : {
-        content : function (){
-            this.$compile(this.$el.querySelector('.modal-body'))
-        },
-        title : function (){
-            this.title = this.title || document.title
-        },
-        toggle : function (){
-            if (this.toggle){
-                this.syncHeight()
-            }
-
-            document.body.style.overflow = this.toggle ? 'hidden' : 'auto'
-        }
-    },
-    methods : {
-        cancelBtnClick : function (){
-            this.toggle = false
-            this.cancelBtnCallback()
-        },
-        okBtnClick : function (){
-            this.toggle = false
-            this.okBtnCallback()
-        },
-        syncHeight : function (){
-            var height = Math.max(this.$$el.find('.modal-dialog').height() + 60, document.documentElement.clientHeight)
-            this.$backdrop.height(height)
-        }
-    },
-    compiled : function (){
-        this.title = this.title || document.title
-
-        this.$$el = $(this.$el)
-        this.$backdrop = this.$$el.find('.vue-modal-backdrop')
-    }
-})
-
-new function (){
-    var str =
-    '<div id="VueUIAlertConfirm">' +
-        '<vue-modal vue-id="VueUIAlert" v-with="config:VueUIAlertConf"></vue-modal>' +
-        '<vue-modal vue-id="VueUIConfirm" v-with="config:VueUIConfirmConf"></vue-modal>' +
-    '</div>'
-    $('body').append(str)
-
-    new Vue({
-        el : '#VueUIAlertConfirm',
-        data : {
-            VueUIAlertConf : {
-                isShowOkBtn : true
-            },
-            VueUIConfirmConf : {
-                isShowOkBtn : true,
-                isShowCancelBtn : true
-            }
-        }
-    })
-
-    var alertVU = VueUI.getComponent('VueUIAlert')
-    var confirmVU = VueUI.getComponent('VueUIConfirm')
-
-    VueUI.alert = function (conf){
-        if ($.type(conf) == 'object'){
-            alertVU.title = conf.title
-            alertVU.content = conf.content || ''
-            alertVU.okBtnCallback = conf.okCallback || VueUI.emptyFunc
-        }
-        else {
-            alertVU.content = conf
-            alertVU.title = document.title
-            alertVU.okBtnCallback = VueUI.emptyFunc
-        }
-        alertVU.toggle = true
-    }
-
-    VueUI.confirm = function (conf){
-        confirmVU.title = conf.title
-        confirmVU.content = conf.content || '',
-        confirmVU.okBtnCallback = conf.okCallback || VueUI.emptyFunc
-        confirmVU.cancelBtnCallback = conf.cancelCallback || VueUI.emptyFunc
-        confirmVU.toggle = true
-    }
 }()
 /*
     Copyright (c) 2015 bravf(bravfing@126.com)
@@ -429,6 +301,134 @@ VueUI.component('vue-datepicker', {
         })
     }
 })
+/*
+    Copyright (c) 2015 bravf(bravfing@126.com)
+*/
+
+VueUI.component('vue-modal', {
+    template :
+        '<div class="modal vue-modal" v-show="toggle">' +
+            '<div class="modal-backdrop fade in vue-modal-backdrop"></div>' +
+            '<div class="modal-dialog" v-style="width:width+\'px\'">' +
+                '<div class="modal-content">' +
+                    '<div class="modal-header">' +
+                        '<button type="button" class="close" v-on="click:toggle=false"><span aria-hidden="true">×</span></button>' +
+                        '<h4 class="modal-title">{{title}}</h4>' +
+                    '</div>' +
+                    '<div class="modal-body">' +
+                        '{{{content}}}' +
+                        '<content></content>' +
+                    '</div>' +
+                    '<div class="modal-footer" v-show="isShowCancelBtn || isShowOkBtn">' +
+                        '<button type="button" class="btn btn-default" v-on="click:cancelBtnClick" v-show="isShowCancelBtn">{{cancelBtnText}}</button>' +
+                        '<button type="button" class="btn btn-primary" v-on="click:okBtnClick" v-show="isShowOkBtn">{{okBtnText}}</button>' +
+                    '</div>' +
+                '</div>' +
+            '</div>' +
+        '</div>'
+    ,
+    data : function (){
+        return {
+            config : {},
+
+            title : '', //标题
+            content : '', //内容
+            toggle : false, //是否显示
+            width : 500, //宽度
+
+            // 按钮相关
+            isShowCancelBtn : false,
+            cancelBtnText : '取消',
+            cancelBtnCallback : VueUI.emptyFunc,
+
+            isShowOkBtn : false,
+            okBtnText : '确认',
+            okBtnCallback : VueUI.emptyFunc,
+        }
+    },
+    watch : {
+        content : function (){
+            this.$compile(this.$el.querySelector('.modal-body'))
+        },
+        title : function (){
+            this.title = this.title || document.title
+        },
+        toggle : function (){
+            if (this.toggle){
+                this.syncHeight()
+            }
+
+            document.body.style.overflow = this.toggle ? 'hidden' : 'auto'
+        }
+    },
+    methods : {
+        cancelBtnClick : function (){
+            this.toggle = false
+            this.cancelBtnCallback()
+        },
+        okBtnClick : function (){
+            this.toggle = false
+            this.okBtnCallback()
+        },
+        syncHeight : function (){
+            var height = Math.max(this.$$el.find('.modal-dialog').height() + 60, document.documentElement.clientHeight)
+            this.$backdrop.height(height)
+        }
+    },
+    compiled : function (){
+        this.title = this.title || document.title
+
+        this.$$el = $(this.$el)
+        this.$backdrop = this.$$el.find('.vue-modal-backdrop')
+    }
+})
+
+new function (){
+    var str =
+    '<div id="VueUIAlertConfirm">' +
+        '<vue-modal vue-id="VueUIAlert" v-with="config:VueUIAlertConf"></vue-modal>' +
+        '<vue-modal vue-id="VueUIConfirm" v-with="config:VueUIConfirmConf"></vue-modal>' +
+    '</div>'
+    $('body').append(str)
+
+    new Vue({
+        el : '#VueUIAlertConfirm',
+        data : {
+            VueUIAlertConf : {
+                isShowOkBtn : true
+            },
+            VueUIConfirmConf : {
+                isShowOkBtn : true,
+                isShowCancelBtn : true
+            }
+        }
+    })
+
+    var alertVU = VueUI.getComponent('VueUIAlert')
+    var confirmVU = VueUI.getComponent('VueUIConfirm')
+
+    VueUI.alert = function (conf){
+        if ($.type(conf) == 'object'){
+            alertVU.title = conf.title
+            alertVU.content = conf.content || ''
+            alertVU.okBtnCallback = conf.okCallback || VueUI.emptyFunc
+        }
+        else {
+            alertVU.content = conf
+            alertVU.title = document.title
+            alertVU.okBtnCallback = VueUI.emptyFunc
+        }
+        alertVU.toggle = true
+    }
+
+    VueUI.confirm = function (conf){
+        confirmVU.title = conf.title
+        confirmVU.content = conf.content || '',
+        confirmVU.okBtnCallback = conf.okCallback || VueUI.emptyFunc
+        confirmVU.cancelBtnCallback = conf.cancelCallback || VueUI.emptyFunc
+        confirmVU.toggle = true
+    }
+}()
 /*
     Copyright (c) 2015 bravf(bravfing@126.com)
 */
